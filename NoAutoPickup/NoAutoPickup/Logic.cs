ba9e52c2fe654a6d7384b099b88905c54c6e1fe5
ChloeIsNobody <64283247+ChloeIsNobody@users.noreply.github.com>
2022-11-08 19:29:20 -0700

    Update Logic.cs

--- NoAutoPickup/NoAutoPickup/Logic.cs
+++ NoAutoPickup/NoAutoPickup/Logic.cs
@@ -1,33 +1,30 @@
-﻿namespace NoAutoPickup
+namespace NoAutoPickup
 {
-    public static class Logic
+  public static class Logic
+  {
+    public static CraftTree.Type GetCraftTreeForGhostCrafter(GhostCrafter ghostCrafter) => ghostCrafter.craftTree;
+
+    public static bool CraftTreeIsModded(CraftTree.Type type)
     {
-        public static CraftTree.Type GetCraftTreeForGhostCrafter(GhostCrafter ghostCrafter)
-        {
-            return ghostCrafter.craftTree;
-        }
-
-        public static bool CraftTreeIsModded(CraftTree.Type type)
-        {
-            var integer = (int)type;
-            return integer < 0 || integer > 10;
-        }
-
-        public static bool ShouldDisableAutoPickupForCraftTree(CraftTree.Type type)
-        {
-            if (CraftTreeIsModded(type))
-            {
-                return Mod.Config.NoAutoPickup_Modded;
-            }
-            switch (type)
-            {
-                case CraftTree.Type.Fabricator:
-                    return Mod.Config.NoAutoPickup_Fabricator;
-                case CraftTree.Type.Workbench:
-                    return Mod.Config.NoAutoPickup_ModificationStation;
-                default:
-                    return Mod.Config.NoAutoPickup_Other;
-            }
-        }
+      int num = (int) type;
+      return num < 0 || num > 10;
     }
+
+    public static bool ShouldDisableAutoPickupForCraftTree(CraftTree.Type type)
+    {
+      if (GameInput.GetButtonHeld(GameInput.Button.Sprint))
+        return false;
+      if (Logic.CraftTreeIsModded(type))
+        return Mod.Config.NoAutoPickup_Modded;
+      switch (type)
+      {
+        case CraftTree.Type.Fabricator:
+          return Mod.Config.NoAutoPickup_Fabricator;
+        case CraftTree.Type.Workbench:
+          return Mod.Config.NoAutoPickup_ModificationStation;
+        default:
+          return Mod.Config.NoAutoPickup_Other;
+      }
+    }
+  }
 }
